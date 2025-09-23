5f18cbc5dc498a49ace3b72bdbbd092d9373f7ff
Giommn <giovannimmneves@gmail.com>
2025-09-23 12:00:29 -0300

    feat: Adição dos métodos PUT e DELETE

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response} from 'express';
 import cors from 'cors';
 import { prisma } from './prisma.js';
 import { _funcionario } from './_funcionario.js';
+import { parse } from 'path';
 
 const app = express();
 const PORT = 3000;
@@ -55,7 +56,8 @@ app.get('/funcionarios', async (req: Request, res: Response) => {
                 email: true,
                 cargo: true,
                 telefone: true,
-                local: true
+                local: true,
+                nivelAcesso: true
             }
         });
         res.status(200).json(funcionarios);
@@ -66,9 +68,76 @@ app.get('/funcionarios', async (req: Request, res: Response) => {
 });
 
 
+app.put('/funcionarios/:id', async (req:Request, res:Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ message: 'O ID do funcionário é obrigatório.'});
+        }
+        
+        const funcionario_id = parseInt(id);
+        const dados_para_atualizar = req.body;
+
+        if (dados_para_atualizar.email && !dados_para_atualizar.email.includes('@'))
+        {
+            return res.status(400).json({ message: 'O email fornecido é inválido!' });
+        }
+
+        const funcionario_atualizado = await prisma.funcionario.update({
+            where: {
+                id: funcionario_id,
+            },
+            data: dados_para_atualizar,
+        });
+
+        const { senhaHash, ...funcionario_sem_senha } = funcionario_atualizado;
+        res.status(200).json(funcionario_sem_senha);
+    } catch (error: any) {
+        if (error.code === 'P2025'){
+            return res.status(404).json({ message: 'Funcionário não encontrado.'});
+        }
+
+        console.error('Erro ao atualizar funcionário:', error);
+        res.status(500).json({ message: 'Ocorreu um erro interno no servidor.'});
+        
+    }
+
+    
+});
+
+
+app.delete('/funcionarios/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ message: 'O ID do funcionário é obrigatório.'});
+        }
+        
+        const funcionario_id = parseInt(id);
+
+        await prisma.funcionario.delete({
+            where: {
+                id: funcionario_id,
+            },
+        });
+
+        res.status(204).send();
+    } catch (error:any) {
+        
+        if (error.code === 'P2025') {
+            return res.status(404).json({ message: 'Ocorreu um erro interno no servidor.'});
+        }
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`servidor pronto e operante em: http://localhost:${PORT}`);
     console.log('Usar o Imsomnia ou Postman pra testar os endpoints:');
     console.log(' - POST http://localhost:3000/funcionarios');
-    console.log(' -GET http://localhost:3000/funcionarios');
+    console.log(' - GET http://localhost:3000/funcionarios');
+    console.log(' - PUT http://localhost:3000/funcionarios');
+    console.log(' - DELETE http://localhost:3000/funcionarios');
 })
