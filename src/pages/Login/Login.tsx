f3c324105f03725e2483e4b3f7d024a9934c43c0
Caio Almeida <104635440+Caio-Almeida4@users.noreply.github.com>
2025-09-28 18:33:33 -0300

    Integração com backend
    Adicionando o envio do CPF e senha digitados para o backend

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,8 +7,40 @@ import img from './fundo.jpg'
 
 
 function Login() {
+  const [cpf, setCpf] = useState('');
+  const [senha, setSenha] = useState('');
+  const [message, setMessage] = useState('');
 
-  const navigate = useNavigate()
+  let navigate = useNavigate()
+
+  const imagemFundo = img
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPost = { cpf, senha };
+
+
+    try {
+      const response = await axios.post(
+        'localhost:3000/login', // Example API endpoint
+        newPost,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      setMessage(`Post created successfully! ID: ${response.data.id}`);
+      setCpf('');
+      setSenha('');
+      navigate('/Teste')
+    } catch (error) {
+      setMessage(`Error creating post: ${error.message}`);
+    }
+    
+
+  };
 
 
   return (
@@ -26,27 +58,27 @@ function Login() {
           <p>Logística Integrada</p>
         </div>
 
-        <form className='form'>
+        <form onSubmit={handleSubmit} className='form'>
 
           <div className='div-input'>
             <label className='input-label'>CPF:</label>
-            <input type="number" className='input'/>
+            <input type="number" value={cpf} onChange={(e) => setCpf(e.target.value)} className='input'/>
 
           </div>
 
 
           <div className='div-input'>
             <label className='input-label'>Senha:</label>
-            <input type='text'className='input'></input>
+            <input type='text'value={senha} onChange={(e) => setSenha(e.target.value)} className='input'></input>
           </div>
 
           <div className='form-button'>
-            <button type="submit" onClick={() => navigate('/Teste')}>Login</button>
+            <button type="submit">Login</button>
           </div>
 
 
         </form>
-
+        {message && <p>{message}</p>}
       </div>
     </div>
   );
