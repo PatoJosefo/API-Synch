163e158a39d6cf74c3787415cafc78f1fd355af2
João Eduardo <joaoeduardooliveirasantos896@gmail.com>
2025-10-27 00:26:27 -0300

    feat/adicionando_frontend_de_cadastro_integrado

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CadastroForm from './components/CadastroForm';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <CadastroForm />
+    </div>
+  );
+};
+
+export default App;
