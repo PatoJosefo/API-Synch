8eadec2295d2f6f7b5f7cb1a03e32fd1883a1c2f
João Eduardo <joaoeduardooliveirasantos896@gmail.com>
2025-10-27 01:12:54 -0300

    feat/adicionanando_frontend_de_formulario_integrado_com_backend

---
+++ formulario-agregado/src/types/index.ts
@@ -0,0 +1,23 @@
+export interface CadastroData {
+  nome: string
+  idade: number
+  genero: string
+  generoOutro?: string
+  cpf: string
+  email: string
+  telefone: string
+  cidade: string
+  estado: string
+  dataNascimento: string 
+  bairro: string
+  rua: string
+  numero: string
+  cep: string
+  complemento?: string
+  curriculo: File | null 
+  fotos: File[]         
+}
+
+export interface ValidationErrors {
+  [key: string]: string
+}
