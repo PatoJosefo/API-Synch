2a3e95561bac4eeb3691b13d1f11469a2f6af78d
Caio Almeida <104635440+Caio-Almeida4@users.noreply.github.com>
2025-11-23 23:38:09 -0300

    Refatoracao e remocao arquivos desnecessarios

--- API-Synch/frontend/src/App.tsx
+++ API-Synch/frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Login from "./pages/Login/Login";
-import Dashboard from "./pages/Dashboard/Dashboard";
 import Calendario from "./pages/Calendario/Calendario.tsx";
 import Notificacoes from "./pages/Notificacoes/Notificacoes";
 import CadastroFuncionario from "./pages/CadastroFuncionario/CadastroFuncionario";
@@ -10,19 +9,18 @@ import FormularioAgregado from "./pages/FormularioAgregado/FormAgregado.tsx";
 function App() {
   return (
     <div className="App">
-        <Routes>
-          {/* Rota Pública - Login */}
-          <Route path="/" element={<Login />} />
+      <Routes>
+        {/* Rota Pública - Login */}
+        <Route path="/" element={<Login />} />
 
-          {/* Rotas Protegidas */}
-          <Route element={<PrivateRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/calendario" element={<Calendario />} />
-            <Route path="/notificacoes" element={<Notificacoes />} /> 
-            <Route path="/cadastro-funcionario" element={<CadastroFuncionario />} /> 
-            <Route path="/formulario-agregado" element={<FormularioAgregado />} />
-          </Route>
-        </Routes>
+        {/* Rotas Protegidas */}
+       <Route element={<PrivateRoute />}>
+          <Route path="/calendario" element={<Calendario />} />
+          <Route path="/notificacoes" element={<Notificacoes />} />
+          <Route path="/cadastro-funcionario" element={<CadastroFuncionario />} />
+          <Route path="/formulario-agregado" element={<FormularioAgregado />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- API-Synch/frontend/src/types/funcionario.ts
+++ API-Synch/frontend/src/types/funcionario.ts
@@ -1,27 +1,29 @@
 export interface EnderecoData {
-  rua: string
-  bairro: string
-  cidade: string
-  estado: string
-  cep: string
-  numero: string
-  complemento: string
+  rua: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  cep: string;
+  numero: string;
+  complemento: string;
 }
 
-export interface FuncionarioData { 
-  nome: string
-  genero: string
-  generoOutro: string
-  cpf: string
-  email: string
-  telefone: string
-  senha: string
-  confirmarSenha: string
-  cargo: string
-  local: string
-  nivelAcesso: number | string 
-  gerente_id: number | null
-  endereco: EnderecoData
+
+export interface FuncionarioData {
+  nome: string;
+  genero: string;
+  generoOutro: string;
+  cpf: string;
+  email: string;
+  telefone: string;
+  dataNascimento: string;
+  endereco: EnderecoData;
+  cargo: string;
+  local: string;
+  nivelAcesso: string;
+  gerente_id: number | null;
+  senha: string;
+  confirmarSenha: string;
 }
 
 export interface ValidationErrors {
